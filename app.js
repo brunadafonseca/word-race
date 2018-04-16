c5c641a8d74a7aca83358dfe199fb433665cac37
bruna <brunadafonseca@gmail.com>
2018-04-16 10:10:40 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,134 @@
+if ('serviceWorker' in navigator) {
+  console.log('serviceworker in navigator')
+  navigator.serviceWorker
+           .register('service-worker.js')
+           .then(() => { console.log('Service Worker Registered') })
+           .catch((err) => { console.log(err.message) })
+}
+
+const list = [
+  {
+    en: "one",
+    pt: "um"
+  },
+  {
+    en: "two",
+    pt: "dois"
+  },
+  {
+    en: "three",
+    pt: "tres"
+  },
+  {
+    en: "four",
+    pt: "quatro"
+  },
+  {
+    en: "five",
+    pt: "cinco"
+  },
+  {
+    en: "six",
+    pt: "seis"
+  },
+  {
+    en: "seven",
+    pt: "sete"
+  },
+  {
+    en: "eight",
+    pt: "oito"
+  },
+  {
+    en: "nine",
+    pt: "nove"
+  },
+  {
+    en: "ten",
+    pt: "dez"
+  }
+]
+
+const INITIAL_STATE = {
+  points: 0,
+  level: 'beginner',
+  wordList: list,
+  word: '',
+  duration: 10,
+  seconds: 10,
+  timerThread: null
+}
+
+class Game {
+  constructor() {
+    this.state = INITIAL_STATE
+  }
+
+  setNewWord() {
+    const index = Math.floor(Math.random()*this.state.wordList.length)
+    this.state.word = this.state.wordList[index]
+    this.state.wordList = this.state.wordList.filter(word => word !== this.state.word)
+    const p = document.getElementById('word')
+    p.innerHTML = this.state.word.en
+  }
+
+  startTimer() {
+    this.state.timerThread = setInterval(() => this.timer(), 1000)
+  }
+
+  timer() {
+    this.state.seconds = this.state.seconds - 1
+      if (this.state.seconds <= 0) {
+        this.start()
+      }
+    document.getElementById('timer').innerHTML = this.state.seconds;
+  }
+
+  checkWord(e) {
+    if (this.state.word.pt === e.target.value.trim().toLowerCase()) {
+      this.state.points = this.state.points + 1
+      e.target.value = ''
+      e.target.classList.remove('wrong')
+      
+      return this.start()
+    }
+
+    e.target.classList.add('wrong')
+  }
+
+  setTimer() {
+    this.state.seconds = this.state.duration
+    document.getElementById('timer').innerHTML = this.state.seconds;
+  }
+
+  start() {
+    clearInterval(this.state.timerThread)
+
+    if (!this.state.wordList.length) {
+      console.log(this.state.points, list.length)
+      console.log('points: ' + this.state.points)
+      return
+    }
+
+    this.setTimer()
+    this.setNewWord()
+    this.startTimer()
+  }
+}
+
+const game = new Game()
+
+function play() {
+  console.log(list2)
+  game.start()
+
+  const guessedWord = document.getElementById('guessed-word')
+  guessedWord.addEventListener('keypress', (e) => {
+      const key = e.which || e.keyCode
+      if (key === 13) {
+        game.checkWord(e)
+    }
+  })
+}
+
+window.play = play
---
+++ service-worker.js
@@ -0,0 +1,119 @@
+// service worker: script that browser runs in the background
+// separate from webpage
+// opens the door to features that don't need webpage or user interaction
+
+const dataCacheName = 'word-race'
+const cacheName = 'word-race'
+const filesToCache = [
+  '/',
+  './assets/images/racing-flag-128.png',
+  './assets/images/racing-flag-256.png',
+  './assets/images/racing-flag-512.png',
+  './assets/css/styles.css',
+  './assets/fonts/WalterTurncoat-Regular.ttf',
+  './index.html',
+  './app.js',
+  './manifest.json'
+]
+
+self.addEventListener('install', (e) => {
+  console.log('[ServiceWorker] Install')
+  // extendable event, ensures sw doesnt install untill code inside it has successfully occurred
+  e.waitUntil( 
+    // create new cache called 'word-race', returns promise
+    caches.open(cacheName)
+      .then((cache) => {
+        console.log('[ServiceWorker] Caching app shell')
+        // addAll takes array of origin-relative URLS to all resources you want to cache
+        return cache.addAll(filesToCache)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  )
+})
+
+self.addEventListener('activate', (e) => {
+  console.log('[ServiceWorker] Activate')
+  e.waitUntil(
+    caches.keys()
+      .then((keyList) => {
+        return Promise.all(keyList.map((key) => {
+          if (key !== cacheName && key !== dataCacheName) {
+            console.log('[ServiceWorker] Removing old cache', key)
+            return caches.delete(key)
+          }
+        }))
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  )
+  return self.clients.claim()
+})
+
+self.addEventListener('fetch', (e) => {
+  console.log('[ServiceWorker] Fetch', e.request.url)
+  // hijack HTTP responses and update them
+  e.respondWith(
+    // responds with resource whose url matches network request url
+    caches.match(e.request)
+      .then((res) => {
+        // return response or fetch default network request for that resource.
+        return res || fetch(e.request).then((res) => {
+          // clones response and puts it in the cache
+          return caches.open(cacheName).then((cache) => {
+
+            cache.put(event.request, res.clone())
+            return res
+          })
+        })
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
+  )
+})
+
+// Service worker:
+// event-driven worker 
+// registered agains origin and path
+// runs in a worker context: has no DOM access, runs on different thread to the main JS that powers the app
+// designed to be fully async
+// act as proxy servers that sit between web apps, 
+// the browser and the networkInterfaces(when available)
+// Intended to:
+// - enable creation of effective offline experiences
+// - intercept network requests and take appropriate action based on
+//   whether network is available
+// - update assets residing on the server 
+// they also allow access to push notifications and background sync APIs
+
+// SW is first registered, if successful, sw is downloaded to client and attempts installation/activation
+// for urls accessed by the user inside the whole origin, or inside a subset specified 
+
+// Download
+// Install
+// Activate
+
+// After that, it's downloaded every 24h or so. May be downloade more frequently
+// but MUST be downloaded every 24h to prevent bad scripts from being annoying for too long
+
+// If downloaded file is new, installation is attempted
+
+// if new sw available, new version is installed in the bg, but not activated (worker in waiting)
+// only activated when there are no pages loaded that still use the old sw
+// when that happens, the new sw activates (becomes active worker)
+// to avoid that, skipWaiting() and existing pages can be claimed by active worker using Clients.claim()
+
+// activate
+// good time to clean up old caches and other things associated with previous version of your sw
+
+// sw responds to requests using FetchEvent. response can be modified using respondWith method
+
+// Interfaces
+
+// Cache
+// represents storage for Req/Res object pairs that are cached as part of sw life cycle
+
+// waitUntil - extendable event
