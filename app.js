ce24314e205c0ce3e0f380fff59268edba261d19
bruna <brunadafonseca@gmail.com>
2018-04-17 11:44:14 +0200

    Add categories

--- app.js
+++ app.js
@@ -6,43 +6,99 @@ if ('serviceWorker' in navigator) {
            .catch((err) => { console.log(err.message) })
 }
 
-// if ('PushManager' in window ) {
-//   window.alert('push manager in window xD')
-// }
-
-const list = [
-  {
-    en: "one",
-    pt: "um",
-    options: [
-      "sete",
-      "um",
-      "quatro"
-    ]
-  },
-  {
-    en: "two",
-    pt: "dois",
-    options: [
-      "dois",
-      "um",
-      "quatro"
-    ]
-  },
-  {
-    en: "three",
-    pt: "tres",
-    options: [
-      "tres",
-      "um",
-      "quatro"
-    ]
-  }
-]
+const list = {
+  animals: [
+    {
+      en: 'cow',
+      pt: 'vaca',
+      options: [
+        'coelho',
+        'ovelha',
+        'vaca'
+      ]
+    },
+    {
+      en: 'rabbit',
+      pt: 'coelho',
+      options: [
+        'coelho',
+        'gato',
+        'arara'
+      ]
+    },
+    {
+      en: 'cat',
+      pt: 'gato',
+      options: [
+        'cachorro',
+        'lagarto',
+        'gato'
+      ]
+    }
+  ],
+  veggies: [
+    {
+      en: 'carrot',
+      pt: 'cenoura',
+      options: [
+        'alface',
+        'cenoura',
+        'acelga'
+      ]
+    },
+    {
+      en: 'carrot',
+      pt: 'cenoura',
+      options: [
+        'alface',
+        'cenoura',
+        'acelga'
+      ]
+    },
+    {
+      en: 'carrot',
+      pt: 'cenoura',
+      options: [
+        'alface',
+        'cenoura',
+        'acelga'
+      ]
+    },
+  ],
+  fruits: [
+    {
+      en: 'orange',
+      pt: 'laranja',
+      options: [
+        'abacate',
+        'laranja',
+        'cereja'
+      ]
+    },
+    {
+      en: 'orange',
+      pt: 'laranja',
+      options: [
+        'abacate',
+        'laranja',
+        'cereja'
+      ]
+    },
+    {
+      en: 'orange',
+      pt: 'laranja',
+      options: [
+        'abacate',
+        'laranja',
+        'cereja'
+      ]
+    }
+  ]
+}
 
 const newGame = {
   points: 0,
-  wordList: list,
+  wordList: [],
   duration: 5,
   seconds: 5,
   word: null,
@@ -50,81 +106,13 @@ const newGame = {
 }
 
 let game = { ...newGame }
-
-function setNewWord() {
-  const index = Math.floor(Math.random()*game.wordList.length)
-  game.word = game.wordList[index]
-  console.log('test')
-  game.wordList = game.wordList.filter(word => word !== game.word)
-
-  const p = document.getElementById('word')
-  p.innerHTML = game.word.en
-
-  const options = game.word.options
-  const btns = document.querySelectorAll('.word-option')
-  btns[0].innerHTML = options[0]
-  btns[1].innerHTML = options[1]
-  btns[2].innerHTML = options[2]
-  btns[0].setAttribute('value', options[0])
-  btns[1].setAttribute('value', options[1])
-  btns[2].setAttribute('value', options[2])
-}
-
-function startTimer() {
-  game.timerThread = setInterval(() => timer(), 1000)
-}
-
-function timer() {
-  game.seconds = game.seconds - 1
-    if (game.seconds <= 0) {
-      start()
-    }
-  document.getElementById('timer').innerHTML = game.seconds;
-}
-
-function checkWord(el) {
-  el.classList.add('active')
-  if (game.word.pt === el.value.toString()) {
-    game.points = game.points + 1
-  }
-
-  el.classList.remove('active')
-  start()
-}
-
-function setTimer() {
-  game.seconds = game.duration
-  document.getElementById('timer').innerHTML = game.seconds;
-}
-
-function start() {
-  console.log(game.wordList)
-  clearInterval(game.timerThread)
-  
-  if (!game.wordList.length) {
-    if (isWinner()) {
-      return window.alert('you won! your score is: ', + game.points)
-    }
-  } 
-  
-  console.log('hi')
-  setTimer()
-  setNewWord()
-  startTimer()
-}
-
-function createNewDiv() {
-  return `<div class="test"></div>`
-}
-
-function isWinner() {
-  return game.points === list.length
-}
+let user = getUser() || {}
 
 // user related
 
-let user = getUser() || null
-if (!!user) {
+if (!!user.name) {
+  addClass('intro', 'hidden')
+  removeClass('game-categories', 'hidden')
   const p = document.createElement('p')
   p.innerHTML = user.name
   document.getElementById('profile').appendChild(p)
@@ -133,6 +121,7 @@ if (!!user) {
 function registerUser(e) {
   localStorage.removeItem('WordRaceUser')
   user.name = e.target.value
+  user.level = 'beginner'
   const newUser = JSON.stringify(user)
   localStorage.setItem('WordRaceUser', newUser)
 }
@@ -148,16 +137,89 @@ function getUser() {
   return JSON.parse(localStorage.getItem('WordRaceUser'))
 }
 
+// game related
+
+function setNewWord() {
+  const index = Math.floor(Math.random()*game.wordList.length)
+  game.word = game.wordList[index]
+  game.wordList = game.wordList.filter(word => word !== game.word)
+
+  const p = document.getElementById('word')
+  p.innerHTML = game.word.en
+
+  const options = game.word.options
+  const btns = document.querySelectorAll('.word-option')
+  btns[0].innerHTML = options[0]
+  btns[1].innerHTML = options[1]
+  btns[2].innerHTML = options[2]
+  btns[0].setAttribute('value', options[0])
+  btns[1].setAttribute('value', options[1])
+  btns[2].setAttribute('value', options[2])
+}
+
+function setTimer() {
+  game.seconds = game.duration
+  document.getElementById('timer').innerHTML = game.seconds;
+}
+
+function startTimer() {
+  game.timerThread = setInterval(() => timer(), 1000)
+}
+
+function timer() {
+  game.seconds = game.seconds - 1
+    if (game.seconds <= 0) {
+      startGame()
+    }
+  document.getElementById('timer').innerHTML = game.seconds;
+}
+
+function checkWord(el) {
+  if (game.word.pt === el.value.toString()) {
+    game.points = game.points + 1
+  }
+
+  startGame()
+}
+
+function startGame() {
+  clearInterval(game.timerThread)
+  
+  if (!game.wordList.length) {
+    if (isWinner()) {
+      addClass('game', 'hidden')
+      removeClass('game-results', 'hidden')
+      window.alert('you won! your score is: ' + game.points)
+      game = { ...newGame }
+      return
+    }
+  } 
+  
+  setNewWord()
+  addClass('game-categories', 'hidden')
+  removeClass('game', 'hidden')
+  setTimer()
+  startTimer()
+}
+
+function isWinner() {
+  return game.points === 3
+}
+
+function play(el) {
+  game = { ...newGame }
+  game.wordList = list[el.value]
+  startGame()
+}
+
 const name = document.getElementById('name')
 name.addEventListener('keypress', (e) => {
   const key = e.which || e.keyCode
   
   if (key === 13) {
     registerUser(e)
-    addClass('intro', 'hide')
-    removeClass('game', 'hide')
-    game = { ...newGame }
-    start()
+    addClass('intro', 'hidden')
+    removeClass('game-categories', 'hidden')
   }
 })
 
@@ -170,35 +232,4 @@ function removeClass(id, oldClass) {
   const element = document.getElementById(id)
   element.classList.remove(oldClass)
 }
-
-// const btns = document.querySelectorAll('.word-option').forEach(btn => {
-//   btn.addEventListener('touchend', fix)
-// })
-
-// function fix() {
-//     var el = this;
-//     var par = el.parentNode;
-//     var next = el.nextSibling;
-//     window.alert(el)
-//     par.removeChild(el);
-//     setTimeout(function() {par.insertBefore(el, next);}, 0)
-// }
-
-
-// const btn = document.getElementsByClassName('btn-start')
-// btn.addEventListener('touchstart', play)
-
-// function play() {
-//   start()
-  
-
-  // const guessedWord = document.getElementById('guessed-word')
-  // guessedWord.addEventListener('keypress', (e) => {
-  //     const key = e.which || e.keyCode
-  //     if (key === 13) {
-  //       checkWord(e)
-  //   }
-  // })
-// }
-
 
